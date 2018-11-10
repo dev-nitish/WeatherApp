14a7813f47d1f57e3db2d8f8a8713f3c553cf40d
Nitish Srivastava <nitish6776@hotmail.com>
2018-11-10 10:08:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,39 +2,66 @@
 const form = document.querySelector('form');
 const loadingImg = document.querySelector('.loading');
 const city = document.querySelector('input[name="city"]');
-const pin = document.querySelector('input[name="pin"]');
-const findId_URL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=oW8yb70tDQESnSGuH08Clb6EFgCl6MPb&q="
+const resultArea = document.querySelector('#tempSection');
+const error = document.querySelector('.error');
+const findId_URL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=oW8yb70tDQESnSGuH08Clb6EFgCl6MPb&q=";
+const getWeather = "http://dataservice.accuweather.com/currentconditions/v1/";
+const API_key = "?apikey=oW8yb70tDQESnSGuH08Clb6EFgCl6MPb";
 
 
 loadingImg.style.display = 'none';
 form.addEventListener('submit', formSubmitted);
 
+
+
 function formSubmitted(event){
 	event.preventDefault();
-	const pinValue = pin.value;
 	const cityValue = city.value;
 
-	
 
+	if(!cityValue){
+		alert('Please Enter City Name!!');
+	 	city.focus();
+	}
+
+	else{
+	
+	resultArea.innerHTML = '';
 	loadingImg.style.display = '';
-	getId();
 	getTemp();
+	}
+
 
 	function getTemp(){
-		
+		var cityId = getId();
+
+
+		cityId.then(function(result) {
+   			const tempURL = `${getWeather}${result}${API_key}`;
+   			// console.log(tempURL);
+   			return fetch(tempURL)
+   				.then(response => response.json(), {'mode': 'no-cors'})
+   				.then(result => {
+   					var temp = result[0].Temperature.Metric.Value + ' &deg;' + result[0].Temperature.Metric.Unit;
+   					
+   					form.reset();
+   					loadingImg.style.display = 'none';
+   					var note = cityValue + ' : ' + temp;
+   					// console.log(note);
+   					resultArea.innerHTML = note;
+			});
+		})
 	}
 
 
 	function getId(){
 		const idurl = `${findId_URL}${cityValue}`;
-		console.log(idurl);
 
 		return fetch(idurl)
 			.then(response => response.json(), {'mode': 'no-cors'})
 			.then(result => {
 				var x = (result[0].Key);
-				console.log(x);
-				return result[0].Key;
+				return parseInt(result[0].Key);
 			});
 	}
 }
